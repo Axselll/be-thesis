cb02ab3cd66918a25511ad16137164fe1b3c584e
Axselll <anasahetapi99@gmail.com>
2022-05-11 18:49:17 +0900

    some changes before pushing to heroku

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as env from 'dotenv'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
