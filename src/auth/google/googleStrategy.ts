dfe0a7f76265b0a77102b2474a70ca8e9330bf05
Axselll <anasahetapi99@gmail.com>
2022-05-11 22:03:17 +0900

    changing callbackurl

--- src/auth/google/googleStrategy.ts
+++ src/auth/google/googleStrategy.ts
@@ -11,7 +11,8 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
         super({
             clientID: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET,
-            callbackURL: 'http://localhost:3000/auth/google/redirect',
+            callbackURL: 'https://api-backend-ta.herokuapp.com/auth/google/redirect',
+            // change to production link heroku 
             scope: ['email', 'profile'],
         });
     }
