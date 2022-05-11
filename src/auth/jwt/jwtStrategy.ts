1bf1fdc69bb1a8adf808b0fc8249b112bbaae887
Axselll <anasahetapi99@gmail.com>
2022-05-11 19:54:45 +0900

    hope this one workv6

--- src/auth/jwt/jwtStrategy.ts
+++ src/auth/jwt/jwtStrategy.ts
@@ -10,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: true,
-            secretOrKey: process.env.SECRET_KEY
+            secretOrKey: 'joemama'
         });
     }
 
