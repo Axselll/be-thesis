ea5ad531cab6ed055e8265a1a2a5a0d249b00566
Axselll <anasahetapi99@gmail.com>
2022-05-11 20:12:13 +0900

    hope this one workv10

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ dotenv.config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
