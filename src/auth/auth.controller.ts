b3ac3e9e34ba0ce40c450e95ce60498f93c9a2d8
Axselll <anasahetapi99@gmail.com>
2022-06-16 20:05:49 +0900

    using cookie session instead of express session

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -19,6 +19,7 @@ export class AuthController {
   @Get('google/redirect')
   async googleAuthRedirect(@Res() res: any): Promise<any> {
     return res.redirect(process.env.ROUTE_TO_FE)
+    // return res.json({ msg: 'sign success' })
   }
 
   @UseGuards(authenticatedGuard)
--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv'
 import * as passport from 'passport'
-import * as session from 'express-session'
+const session = require('cookie-session');
 dotenv.config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(session({
-    secret: process.env.SECRET_KEY,
-    resave: false,
-    saveUninitialized: false,
-  }))
-
-
-  app.use(passport.initialize())
-  app.use(passport.session())
-
   app.enableCors({
-    allowedHeaders: "*",
-    origin: "*",
+    origin: '*',
     credentials: true
   });
-
+  app.use(session({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [process.env.SECRET_KEY]
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
